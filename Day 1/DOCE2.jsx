b44d54cdb3f2647c9fcaee6db6b357f860c366c5
Pragatheesh S <150768626+praxx26@users.noreply.github.com>
2024-08-07 22:00:40 +0530

    Add files via upload

---
+++ Day 1/DOCE2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Day1class2 =()=>
+{
+    return(
+        <div>
+            <p>This is the World Functional Conponents</p>
+        </div>
+    );
+
+};
+export default Day1class2;
