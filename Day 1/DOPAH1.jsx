3d34eb40e45fb61d8bdf40822ee1dd0ac82b267a
Pragatheesh S <150768626+praxx26@users.noreply.github.com>
2024-08-07 22:22:34 +0530

    Add files via upload

---
+++ Day 1/DOPAH1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const Day1pah1 =()=>
+{
+    return(
+        <div>
+            <p>This is team Function Component</p>
+        </div>
+    );
+
+};
+export default Day1pah1;
